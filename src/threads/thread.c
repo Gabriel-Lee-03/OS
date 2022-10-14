bcef181f42112f5477776dd10af53a087a33f54f
Tsz Wong <tcw121@aube.doc.ic.ac.uk>
2022-10-14 12:02:14 +0100

    [everyone] initialise thread_get_priority() and thread_ser priority()

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -71,6 +71,10 @@ static void schedule (void);
 void thread_schedule_tail (struct thread *prev);
 static tid_t allocate_tid (void);
 
+//Task 1
+static bool less_priority (const struct list_elem *a, 
+    const struct list_elem *b, void *aux UNUSED);
+
 /* Initializes the threading system by transforming the code
    that's currently running into a thread.  This can't work in
    general and it is possible in this case only because loader.S
@@ -216,6 +220,9 @@ thread_create (const char *name, int priority,
   /* Add to run queue. */
   thread_unblock (t);
 
+  if (t -> priority > thread_get_priority)
+    schedule();
+
   return tid;
 }
 
@@ -252,7 +259,7 @@ thread_unblock (struct thread *t)
 
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
-  list_push_back (&ready_list, &t->elem);
+  list_insert_ordered (&ready_list, &t->elem, less_priority, NULL);
   t->status = THREAD_READY;
   intr_set_level (old_level);
 }
@@ -323,10 +330,18 @@ thread_yield (void)
 
   old_level = intr_disable ();
   if (cur != idle_thread) 
-    list_push_back (&ready_list, &cur->elem);
+    list_insert_ordered (&ready_list, &cur->elem, less_priority, NULL);
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
+}
+
+//Task 1
+static bool less_priority (const struct list_elem *a, 
+    const struct list_elem *b, void *aux UNUSED) {
+  struct thread *thread_a = list_entry(a, struct thread, elem);
+  struct thread *thread_b = list_entry(b, struct thread, elem);
+  return ((thread_a -> priority) < (thread_b -> priority));
 }
 
 /* Invoke function 'func' on all threads, passing along 'aux'.
@@ -351,6 +366,9 @@ void
 thread_set_priority (int new_priority) 
 {
   thread_current ()->priority = new_priority;
+  if (list_entry(list_front(&ready_list), struct thread, elem) 
+      -> priority > new_priority) 
+    thread_yield();
 }
 
 /* Returns the current thread's priority. */
