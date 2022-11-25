a20117cd48e64278d308b5af6e57396cf159ebfc
Tsz Wong <tcw121@aube.doc.ic.ac.uk>
2022-11-25 13:12:41 +0000

    [tcw121][jy2221] Implemented frame_alloc() in frame.c

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -5,7 +5,7 @@
 #define MAX_FRAME_SIZE (1024 * 1024)
 
 static struct frame_table_entry* entries;
-static int size;
+static int frame_size;
 
 void frame_init() {
     void* frame_ptr;
@@ -14,11 +14,26 @@ void frame_init() {
     if (entries != NULL) {
         frame_ptr = palloc_get_page(PAL_USER);
         while (frame_ptr != NULL) {
-            size++;
-            entry = &entries[size];
+            frame_size++;
+            entry = &entries[frame_size];
             entry->frame_ptr = frame_ptr;
             entry->page = NULL;
             frame_ptr = palloc_get_page(PAL_USER);
         }
     }
 }
+
+struct frame_table_entry* frame_alloc(struct page *page) {
+    struct frame_table_entry* entry;
+    for (int i = 0; i < frame_size; i++) {
+        entry = &entries[i];
+        /* Find a free frame */
+        if (entry->page == NULL) {
+            entry->page = page;
+            return entry;
+        }
+    }
+    return NULL;
+}
+
+
