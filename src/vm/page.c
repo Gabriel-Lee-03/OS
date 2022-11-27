40005d23f808ff5f95f91629df121ebc75cbdee9
Tsz Wong <tcw121@aube.doc.ic.ac.uk>
2022-11-27 17:04:31 +0000

    Added supplemental page table to page.h

--- src/vm/page.c
+++ src/vm/page.c
@@ -1,3 +1,9 @@
 #include <stdio.h>
 #include "vm/page.h"
 
+struct supp_page_table_entry* page_info_lookup(void *);
+
+struct supp_page_table_entry* page_info_lookup(void *user_vaddr) {
+    struct supp_page_table_entry entry;
+};
+
--- src/vm/page.h
+++ src/vm/page.h
@@ -1,11 +1,18 @@
 #ifndef VM_PAGE_H
 #define VM_PAGE_H
 
-#include <inttypes.h>
+#include <stdbool.h>
 
 struct page {
     struct thread* owner;
     struct frame_table_entry* entry;
+};
+
+struct supp_page_table_entry {
+    void *user_vaddr;
+    bool no_data;       /* Not expect any data */
+    bool kernel_vm;     /* Lies within kernel virtual memory */
+    bool read_only;     /* Read-only page */
 };
 
 #endif /* vm/page.h */
