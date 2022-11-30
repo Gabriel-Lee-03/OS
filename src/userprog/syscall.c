f63544c3d16b6fd7ee9ec047affd4b0a01a15e91
Tsz Wong <tcw121@aube.doc.ic.ac.uk>
2022-11-30 13:53:56 +0000

    Debugged syscall_handler() in syscall.c, sc-bad-arg passed

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -74,6 +74,9 @@ syscall_handler (struct intr_frame *f UNUSED)
   case SYS_EXIT:
     check_mem_access(f->esp + 1);
     status = *(((int*)f->esp) + 1);
+    if (status < -1) {
+      status = -1;
+    }
     sc_exit(status);
     break;
 
